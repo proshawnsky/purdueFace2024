94392b33a82c9167c2453876ccc3c81e35ba74f1
proshawnsky <50030056+proshawnsky@users.noreply.github.com>
2024-03-22 16:50:16 -0400

    Batches.py
    This version successfully uses an Image loader (only tested on the small dataset so far)

--- main.py
+++ main.py
@@ -1,84 +1,54 @@
 import cv2
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from google.colab import drive
 import os
 import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+#from tensorflow.keras.preprocessing.image import ImageDataGenerator, image_dataset_from_directory
 
+# Define folder paths
+pics_path = r'C:\Users\shawn\Documents\Kaggle\train_small'
+kaggle_path = r'C:\Users\shawn\Documents\Kaggle'
+processed_path = r'C:\Users\shawn\Documents\Kaggle\train_sorted'
+validation_path = r'C:\Users\shawn\Documents\Kaggle\validation'
 
-drive.mount('/content/drive')
-pics_path = '/content/drive/My Drive/ECE_50024_Kaggle/pics'
-kaggle_path = '/content/drive/My Drive/ECE_50024_Kaggle'
-cropped_path = '/content/drive/My Drive/ECE_50024_Kaggle/cropped'
-#pics_path = r'C:\Users\shawn\Documents\Kaggle\pics'
-#kaggle_path = r'C:\Users\shawn\Documents\Kaggle'
-#cropped_path = r'C:\Users\shawn\Documents\Kaggle\cropped'
+train_data_dir = processed_path
+validation_data_dir = validation_path
+nb_train_samples = 3932
+nb_validation_samples = 2034
+epochs = 30
+img_width = 64
+img_height = 64
+batch_size = 200
 
-# import face recognition tool
-os.chdir(kaggle_path)
-face_cascade = cv2.CascadeClassifier('face_rec.xml')
+# Data generator
+img_width, img_height = 64, 64
 
-# get file names and celeb names for pictures 
-allCategories = pd.read_csv('category.csv')
-categories = allCategories['Category'].tolist()
-trainData = pd.read_csv('train_small.csv')
-allFileNames = (trainData['File Name']).tolist()
+train_ds = tf.keras.utils.image_dataset_from_directory(
+  processed_path,
+  label_mode='int',
+  color_mode="grayscale",
+  labels = 'inferred',
+#   validation_split=0.2,
+#   subset="training",
+  shuffle=True,
+  seed=123,
+  image_size=(64, 64),
+  batch_size=100)
+input_shape = (64, 64, 1)
+print(train_ds)
 
-# get list of available pictures
-os.chdir(pics_path)
-pics = os.listdir(pics_path)
-
-# for each picture, ...
-
-x_train = []
-y_train = []
-i = 0
-for pic in pics:
-    os.chdir(pics_path)
-    img = cv2.imread(pic)                                         # load the picture into an array
-    
-    if img is not None:                                           # only continue if the picture was succesfully loaded
-      gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)                # convert the image to grayscale
-      faces = face_cascade.detectMultiScale(gray, 1.2, 4)         # attempt to detect any face(s)
-
-      if len(faces) > 0:                                          # if any faces were detected, ...
-          x, y, w, h = faces[0,:]                                 # extract the position and size of the face
-          cropped = gray[y:y+h, x:x+w]                            # store a cropped version of the image using the face rectangle
-          cropped_size = 64                                      # set the size of all images
-          resized = cv2.resize(cropped, (cropped_size, cropped_size)) # resize all images to be the same dimensions
-          x_train.append(resized)
-          
-          celebName = trainData['Category'][allFileNames.index(pic)]
-          celebID = categories.index(celebName)
-          y_train.append(celebID)
-          i += 1
-          if (i%100 ==0):
-              print(i)
-
-x_train_temp = np.stack(np.array(x_train))/255
-y_train_temp = np.stack(y_train)
-
-idxToTrain = 700 # number of training samples
-x_test = x_train_temp[idxToTrain:,:,:]
-x_train = x_train_temp[:idxToTrain,:,:]
-y_test = y_train_temp[idxToTrain:]
-y_train = y_train_temp[:idxToTrain]
-#__________________________________________________________________________________________________________________________________
-
-x_train = np.expand_dims(x_train, -1)
-x_test = np.expand_dims(x_test, -1)
-print("x_train shape:", x_train.shape)
-print(x_train.shape[0], "train samples")
-print(x_test.shape[0], "test samples")
-
-#_______________________________________________________________________________________________________________________________
-
-num_classes = 100
-input_shape = (cropped_size, cropped_size, 1)
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_validation = keras.utils.to_categorical(y_test, num_classes)
+if K.image_data_format() == 'channels_first':
+    input_shape = (1, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 1)
 
 model = keras.Sequential(
     [
@@ -87,46 +57,47 @@ model = keras.Sequential(
         layers.MaxPooling2D(pool_size=(2, 2)),
         layers.Conv2D(64, kernel_size=(5, 5), activation="relu"),
         layers.MaxPooling2D(pool_size=(2, 2)),
-        #layers.Conv2D(128, kernel_size=(3, 3), activation="relu"),
+        #layers.Conv2D(256, kernel_size=(3, 3), activation="relu"),
         #layers.MaxPooling2D(pool_size=(2, 2)),
         layers.Flatten(),
         # layers.Dropout(0.5),
-        layers.Dense(num_classes, activation="softmax"),
+        layers.Dense(100, activation="softmax"),
     ]
 )
 model.summary()
 
-#__________________________________________________________________________________________________
-batch_size = 128
-epochs = 15
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
 
-model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+# this is the augmentation configuration we will use for training
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
 
-#score = model.evaluate(x_test, y_test, verbose=0)
-#print("Test loss:", score[0])
-#print("Test accuracy:", score[1])
-import matplotlib.pyplot as plt
+# this is the augmentation configuration we will use for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1./ 255)
 
-# plot the result
-idx = 15
-plt.figure()
-plt.imshow(x_test[idx,:,:,0], cmap='gray')
-# plt.figure()
-yhat = model.predict(x_test[idx:(idx+1)])
-# plt.bar(np.arange(100),yhat[0])
-# plt.xlim([0,100])
-# plt.title('Prediction probability')
-# plt.grid()
-sorted(zip(yhat[0], categories), reverse=True)[:3]
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    color_mode="grayscale",
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
 
-len(x_test)
-max_test=np.zeros(y_test.shape)
-for test in range(len(x_test)):
-  yhat = model.predict(x_test[test:(test+1)])
-  temp = yhat[0]
-  max_test[test] = max(temp)
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    color_mode="grayscale",
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='categorical')
 
-plt.figure()
-plt.plot(range(len(x_test)), max_test)
-plt.xlim((0,100))
+model.fit(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size)
